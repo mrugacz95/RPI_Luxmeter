9d3f55b3477c515a3032acff65ae50790ede981e
Marcin Mrugas <mrugacz95@gmail.com>
2017-04-23 01:17:19 +0200

    Server fixes

--- flaskServer/flaskServer.py
+++ flaskServer/flaskServer.py
@@ -26,11 +26,6 @@ def root():
     return app.send_static_file('index.html')
 
 
-@app.route('/<path:path>')
-def send_js(path):
-    return app.send_static_file(path)
-
-
 @app.route('/api')
 def hello_world():
     cur = mysql.connection.cursor(cursorclass=DictCursor)
--- flaskServer/static/charts.js
+++ flaskServer/static/charts.js
@@ -1,34 +1,48 @@
 //google.charts.load('current', {packages: ['corechart']});
-google.charts.load("visualization", "1", { packages: ["corechart"] });
+google.charts.load("visualization", "1", {packages: ["corechart"]});
 google.charts.setOnLoadCallback(getData);
-function getData(){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        drawChart(JSON.parse(this.response));
-        $('#response').get(0).innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/api", true);
-  xhttp.send();
+function getData() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            drawChart(JSON.parse(this.response));
+        }
+    };
+    xhttp.open("GET", "/api", true);
+    xhttp.send();
 }
 function drawChart(data) {
-    var dataTable = new google.visualization.DataTable();
-    dataTable.addColumn('date', 'Time');
-    dataTable.addColumn('number', 'Lux');
-    data = data.map((obj) =>   [new Date(obj['date']) , obj['value']]);
-    console.log(data);
-    dataTable.addRows(data);
-    var chart = new google.visualization.LineChart($('#chart').get(0));
-    var options = {
+    let dataTable = new google.visualization.DataTable();
+    const options = {
         vAxis: {
-            scaleType: 'log' ,
-            title: 'Lux' },
+            scaleType: 'log',
+            title: 'Lux'
+        },
         legend: {position: 'none'},
+        curveType: 'function',
+        chartArea: {
+            top: 55,
+            height: '40%'
+        },
         hAxis: {
             title: "Date",
-            gridlines: { count: 3, color: '#CCC' },
-            format: 'dd-MMM-yyyy hh:mm:ss'
-        }};
+            gridlines: {count: 20, color: '#CCC'},
+            format: 'hh:mm:ss',
+            ticks: data.map((obj) => new Date(obj['date'])),
+            slantedTextAngle: 45,
+            slantedText: true
+        },
+        backgroundColor: '#fff',
+        height : 500,
+        width : '100%'
+
+
+    };
+    dataTable.addColumn('date', 'Time');
+    dataTable.addColumn('number', 'Lux');
+    data = data.map((obj) => [new Date(obj['date']), obj['value']]);
+    dataTable.addRows(data);
+    let chart = new google.visualization.LineChart($('#chart').get(0));
+
     chart.draw(dataTable, options);
 }
