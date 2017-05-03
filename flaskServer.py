a2203f8c3b1a00020ac3cbab603296f114e03a0d
Marcin Mrugas <mrugacz95@gmail.com>
2017-05-04 00:19:50 +0200

    Making measurements

--- flaskServer.py
+++ flaskServer.py
@@ -1,3 +1,6 @@
+import argparse
+import smbus
+
 from flask import Flask, jsonify, url_for, send_from_directory, render_template
 from flask_mysqldb import MySQL
 from MySQLdb.cursors import DictCursor
@@ -10,6 +13,9 @@ app.config['MYSQL_DB'] = 'rpi_luxmeter'
 app.config['MYSQL_HOST'] = 'localhost'
 
 mysql.init_app(app)
+
+DEVICE = 0x23
+ONE_TIME_HIGH_RES_MODE_1 = 0x20
 
 
 def date_handler(obj):
@@ -30,5 +36,31 @@ def hello_world():
     return jsonify(rv)
 
 
+def convertToNumber(data):
+
+    return (data[1] + (256 * data[0])) / 1.2
+
+
+def make_measurement(addr=DEVICE):
+    data = bus.read_i2c_block_data(addr, ONE_TIME_HIGH_RES_MODE_1)
+    value = convertToNumber(data)
+    print(value)
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """INSERT INTO 
+            measurements (value)
+        VALUES (%.f)""", (value))
+    mysql.connection.commit()
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m',
+                        '--measurement',
+                        type=bool,
+                        help="Set True to make single measurement")
+    args = parser.parse_args()
+    if args.measurement:
+        make_measurement()
+    else:
+        app.run(debug=True)
