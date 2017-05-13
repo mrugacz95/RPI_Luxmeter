0f312de7b6d7f92379da75a618bac196385fba12
Marcin Mrugas <mrugacz95@gmail.com>
2017-05-14 01:59:26 +0200

    Get data form date to date

--- server.py
+++ server.py
@@ -6,16 +6,18 @@ from flask import Flask, jsonify, url_for, send_from_directory, render_template,
 from flask_mysqldb import MySQL
 from MySQLdb.cursors import DictCursor
 
+import measurement
+
 mysql = MySQL()
 app = Flask(__name__)
 
 config = configparser.ConfigParser()
 config.read('databaseconfig.ini')
 section = 'DEFAULT'
-app.config['MYSQL_USER'] = config.get(section, 'user')
-app.config['MYSQL_PASSWORD'] = config.get(section, 'passwd')
-app.config['MYSQL_DB'] = config.get(section, 'db')
-app.config['MYSQL_HOST'] = config.get(section, 'host')
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'marcin'
+app.config['MYSQL_DB'] = 'rpi_luxmeter'
+app.config['MYSQL_HOST'] = 'localhost'
 
 mysql.init_app(app)
 
@@ -32,12 +34,20 @@ def root():
 
 
 @app.route('/api')
-def hello_world():
+def measurements():
+    date_from = request.args.get('from')
+    date_to = request.args.get('to')
     cur = mysql.connection.cursor(cursorclass=DictCursor)
     date_begin = request.args.get('user')
     date_end = request.args.get('user')
-    cur.execute('SELECT * FROM measurements WHERE date > NOW() - INTERVAL 1 DAY ORDER BY date')
+
+    if date_from == None or date_to == None:
+        cur.execute('SELECT * FROM measurements WHERE date > NOW() - INTERVAL 1 DAY ORDER BY date')
+    else:
+        cur.execute('SELECT * FROM measurements WHERE date >  STR_TO_DATE(%s, "%d/%m/%Y") AND date <  STR_TO_DATE(%s, "%d/%m/%Y") DAY ORDER BY date', date_from, date_to)
+    print(cur._executed)
     rv = cur.fetchall()
+    measurement.make_measurement()
     return jsonify(rv)
 
 
